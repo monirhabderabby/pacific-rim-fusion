05aa208a58f0a619a8d117fa31ca070472c42e57
mohammad-anar <ai.anarul4232@gmail.com>
2025-01-02 14:38:48 +0600

    Category sidebar

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home/home';
+import Categories from './pages/categories/categories';
 
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home/>
+  },
+  {
+    path: '/categories',
+    element: <Categories/>
   },
 ]);
 
