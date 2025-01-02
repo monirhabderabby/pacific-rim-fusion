b9bdf3babb049a15c4329604ec6dcc0e157a50dd
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-01-02 11:28:19 +0600

    added: colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          green: "#1D4C1F",
+          "green-hover": "#2A6C2D",
+          light: "#EAF0EA",
+          black: "#000000",
+          "black-light": "#1A1A1A",
+        },
+      },
+    },
   },
   plugins: [],
 };
