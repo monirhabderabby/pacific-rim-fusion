9fc354497928b772ccfa8b313645f0006afbdda5
sozibhossain <sozibhossain69@gmail.com>
2025-01-02 14:19:29 +0600

    Our Featured Products

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home/home';
+import HomeAfter from './pages/HomeAfter/HomeAfter';
 
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home/>
+  },
+  {
+    path: '/homeafter',
+    element: <HomeAfter/>
   },
 ]);
 
